8374c9caa1ee7906999ab5d020ae28c93c771c32
TheOriginalTiger <vvp.vovarulit@gmail.com>
2021-10-22 16:01:40 +0700

    added example separation

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from app import app
+from flask import render_template
+
+
+@app.route("/exmaple1")
+def example1():
+    user = { 'name':"Igor Chiesov"}
+    return render_template("index.html", title = "pizdec, html", user=user )
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello world"
+    return "This Is just a start page. Try to do /example1, 2, 3"
+

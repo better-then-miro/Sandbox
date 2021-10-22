c4b5fcc6d055877c01ca032c9fdcce47c08afd24
TheOriginalTiger <vvp.vovarulit@gmail.com>
2021-10-22 17:14:08 +0700

    socketIO added

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,30 @@
-from app import app
+import socketio
+from app import app, socketio
 from flask import render_template
+from flask_socketio import emit
 
 
-@app.route("/exmaple1")
-def example1():
-    user = { 'name':"Igor Chiesov"}
-    return render_template("index.html", title = "pizdec, html", user=user )
-
+posts = []
+#simple example of how flask work
 @app.route("/")
 @app.route("/index")
 def index():
     return "This Is just a start page. Try to do /example1, 2, 3"
 
+
+#here we introduce templates
+@app.route("/example1")
+def example1():
+    user = { 'name':"Igor Chiesov"}
+    return render_template("index.html", exNumber = "1", user=user, posts = posts )
+
+
+@app.route("/example2")
+def example2():
+    return render_template( 
+        "textField.html",  exNumber = "2"
+    )
+
+@socketio.on("getMessage", namespace="Main")
+def getMessage(msg):
+    print(msg)

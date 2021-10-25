ce9bf8bc1a12f9cf74bd74b1354869a303a748b6
TheOriginalTiger <vvp.vovarulit@gmail.com>
2021-10-25 18:40:31 +0700

    more comments....

--- app/static/example3.js
+++ app/static/example3.js
@@ -1,11 +1,14 @@
 
 async function send_messageExample3()
 {
-
+    //строим урл для запроса. итоговое значение будет 
+    //  http://127.0.0.1:5000/example3
     const url = location.protocol + '//' + document.domain + ':' + location.port + '/example3';
+    // вытаскиваем данные из текстового поля по id
     const data = { msg: document.getElementById("MessageBox").value };
 
     try {
+        //строим и отправляем POST-запрос на запрос 
         const response = await fetch(url, {
             method: 'POST', // или 'PUT'
             body: JSON.stringify(data), // данные могут быть 'строкой' или {объектом}!
@@ -13,6 +16,7 @@ async function send_messageExample3()
             'Content-Type': 'application/json'
             }
         });
+        //пишем в консоль  браузера ответ от сервера
         if (response.ok) {
             console.log(response)
         }    
